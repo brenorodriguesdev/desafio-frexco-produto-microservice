657d70a6cadbb653802eb332b7fe898ade884629
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 07:02:41 -0300

    feat: add atualizar produto controller

---
+++ src/presentation/controllers/atualizar-produto.ts
@@ -0,0 +1,20 @@
+import { AtualizarProdutoUseCase } from "../../domain/useCases/atualizar-produto";
+import { Validator } from "../../validation/contracts/validator";
+import { AMQPRequest } from "../contracts/amqp";
+import { Controller } from "../contracts/controller";
+
+export class AtualizarProdutoController implements Controller {
+    constructor (private readonly validator: Validator, private readonly atualizarProdutoUseCase: AtualizarProdutoUseCase) {}
+    async handle(AMQPRequest: AMQPRequest): Promise<any> {
+        const error = this.validator.validate(AMQPRequest.payload)
+        if (error) {
+            throw error
+        }
+        const { idCategoria, nome, id } = AMQPRequest.payload
+        await this.atualizarProdutoUseCase.atualizar({
+            id,
+            nome,
+            idCategoria
+        })
+    }
+}
