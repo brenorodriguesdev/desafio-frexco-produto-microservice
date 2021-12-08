faa8752aee425cc43151ed81108604cf52f117af
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 06:45:35 -0300

    feat: implement categoria repository

---
+++ src/infra/categoria-repository.ts
@@ -0,0 +1,10 @@
+import { getRepository } from "typeorm"
+import { CategoriaRepository } from "../data/contracts/categoria-repository"
+import { Categoria } from "../data/entities/categoria"
+
+export class CategoriaRepositoryTypeORM implements CategoriaRepository {
+    async getById(id: number): Promise<Categoria> {
+        const categoriaRepository = getRepository(Categoria)
+        return await categoriaRepository.findOne(id)
+    }
+}
