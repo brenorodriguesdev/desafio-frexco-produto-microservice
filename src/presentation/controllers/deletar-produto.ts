e7bcc2aac955fb71fa338d1a634a836e586da92d
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 07:02:54 -0300

    feat: add deletar produto controller

---
+++ src/presentation/controllers/deletar-produto.ts
@@ -0,0 +1,16 @@
+import { DeletarProdutoUseCase } from "../../domain/useCases/deletar-produto";
+import { Validator } from "../../validation/contracts/validator";
+import { AMQPRequest } from "../contracts/amqp";
+import { Controller } from "../contracts/controller";
+
+export class DeletarProdutoController implements Controller {
+    constructor (private readonly validator: Validator, private readonly deletarProdutoUseCase: DeletarProdutoUseCase) {}
+    async handle(AMQPRequest: AMQPRequest): Promise<any> {
+        const error = this.validator.validate(AMQPRequest.payload)
+        if (error) {
+            throw error
+        }
+        const { id } = AMQPRequest.payload
+        await this.deletarProdutoUseCase.deletar(id)
+    }
+}
