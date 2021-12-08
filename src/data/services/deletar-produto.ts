72bbbe49dd23b18d825127716a101c63a07e2afa
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 06:41:10 -0300

    feat: add deletar produto service

---
+++ src/data/services/deletar-produto.ts
@@ -0,0 +1,15 @@
+import { DeletarProdutoUseCase } from "../../domain/useCases/deletar-produto";
+import { ProdutoRepository } from "../contracts/produto-repository";
+
+export class DeletarProdutoService implements DeletarProdutoUseCase {
+    constructor (private readonly produtoRepository: ProdutoRepository) {}
+    async deletar (id: number): Promise<void | Error> {
+
+        const produto = await this.produtoRepository.getById(id)
+        if (!produto) {
+            return new Error('Esse produto não foi encontrado!')
+        }
+
+        await this.produtoRepository.deleteById(id)
+    }
+}
