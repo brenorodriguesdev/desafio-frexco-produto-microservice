f90ed6a6e5357ad7d911bcffc127728812b47efc
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 07:02:48 -0300

    feat: add criar produto controller

---
+++ src/presentation/controllers/criar-produto.ts
@@ -0,0 +1,20 @@
+import { CriarProdutoUseCase } from "../../domain/useCases/criar-produto";
+import { Validator } from "../../validation/contracts/validator";
+import { AMQPRequest } from "../contracts/amqp";
+import { Controller } from "../contracts/controller";
+
+export class CriarProdutoController implements Controller {
+    constructor (private readonly validator: Validator, private readonly criarProdutoUseCase: CriarProdutoUseCase) {}
+    async handle(AMQPRequest: AMQPRequest): Promise<any> {
+        const error = this.validator.validate(AMQPRequest.payload)
+        if (error) {
+            throw error
+        }
+        const { idCategoria, nome } = AMQPRequest.payload
+        const produto = await this.criarProdutoUseCase.criar({
+            nome,
+            idCategoria
+        })
+        return produto
+    }
+}
