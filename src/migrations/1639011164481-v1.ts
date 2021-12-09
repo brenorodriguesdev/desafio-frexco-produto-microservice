86532d3f20fe10a5878324f2df8c37d5feabdba2
“Breno <brenorodriguesdev@gmail.com>
2021-12-08 22:00:30 -0300

    chore: add migrations

---
+++ src/migrations/1639011164481-v1.ts
@@ -0,0 +1,66 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey} from "typeorm";
+
+export class v11639011164481 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name: "categoria",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true
+                },
+                {
+                    name: "nome",
+                    type: "varchar",
+                }
+            ]
+        }), true);
+
+        await queryRunner.createTable(new Table({
+            name: "produto",
+            columns: [
+                {
+                    name: "id",
+                    type: "int",
+                    isPrimary: true,
+                    isGenerated: true
+                },
+                {
+                    name: "nome",
+                    type: "varchar",
+                }
+            ]
+        }), true);
+
+        await queryRunner.addColumn("produto", new TableColumn({
+            name: "idCategoria",
+            type: "int"
+        }));
+
+        await queryRunner.createForeignKey("produto", new TableForeignKey({
+            columnNames: ["idCategoria"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "categoria",
+            onDelete: "CASCADE"
+        }));
+        
+        await queryRunner.query("insert into categoria values (1,'Categoria 1')");
+        await queryRunner.query("insert into categoria values (2,'Categoria 2')");
+        await queryRunner.query("insert into categoria values (3,'Categoria 3')");
+        await queryRunner.query("insert into categoria values (4,'Categoria 4')");
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+        const table = await queryRunner.getTable("produto");
+        const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf("idCategoria") !== -1);
+        await queryRunner.dropForeignKey("produto", foreignKey);
+
+        await queryRunner.dropTable("produto");
+    }
+}
