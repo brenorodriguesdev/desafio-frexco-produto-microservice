b9f7129a767286550a790d5be799cce4553d9a20
“Breno <brenorodriguesdev@gmail.com>
2021-12-10 22:29:41 -0300

    feat: add buscar produto queue

--- src/main/queues/produto.ts
+++ src/main/queues/produto.ts
@@ -1,5 +1,6 @@
 import { adaptQueue } from "../adapters/amqp-controller";
 import { makeAtualizarProdutoController } from "../factories/controllers/atualizar-produto";
+import { makeBuscarProdutoController } from "../factories/controllers/buscar-produto";
 import { makeCriarProdutoController } from "../factories/controllers/criar-produto";
 import { makeDeletarProdutoController } from "../factories/controllers/deletar-produto";
 import { makeListarProdutosController } from "../factories/controllers/listar-produtos";
@@ -9,4 +10,5 @@ export default (channel: any): void => {
     channel.consume('atualizar-produto', adaptQueue(channel, makeAtualizarProdutoController()));
     channel.consume('deletar-produto', adaptQueue(channel, makeDeletarProdutoController()));
     channel.consume('listar-produtos', adaptQueue(channel, makeListarProdutosController()));
+    channel.consume('buscar-produto', adaptQueue(channel, makeBuscarProdutoController()));
 }
